a9c06ee9a667a1facc14efeb9fed6696e3cbb07d
nicolewallace09 <nicole.elisaw@gmail.com>
2020-04-05 09:40:31 -0700

    start button function/show questions

--- assets/script.js
+++ assets/script.js
@@ -1,49 +1,70 @@
-start-btn.addEventListener('click', startQuiz)
+const startButton = document.getElementById('start-btn');
+const startScreen = document.getElementById('start-screen');
+const questionsContainer = document.getElementById('question-container');
+const questionsEl = document.getElementById('question')
+const answerButton = document.getElementById('choices')
+
+let randomQuestions, setQuestions
+
+startButton.addEventListener('click', startQuiz)
 
 // start quiz
-function startQuiz(questions) {
-    counterTime();
-    timer = 60 ;
+function startQuiz() {
+    resetAnswers ();
+    console.log("started")
+    //counterTime();
+    //timer = 60 ;
 
-    start-btn.classList.add('hide');
-    questions.classList.remove('hide');
-    
-    // Loop questions
-    for(var i=0; i < questions.Length; i++){
-        var answer = confirm(questions[i].q);
+    startButton.classList.add('hide');
+    startScreen.classList.add('hide');
+    randomQuestions = questions.sort(() => Math.random() -.5)
+    setQuestions = 0 
+    questionsContainer.classList.remove('hide');
+    nextQuestion();
+}
 
-    // Correct answer
-    if (
-        (answer === true && questions[i].a === 't') ||
-        (answer === false && questions[i].a === 'f')
-    ){
-        // Increase score
-        score++;
-        alert('Correct!');
-     } else {
-         alert('Wrong!');
-     }
-     
-// Show score 
-alert('You got ' + score + '/' + questions.length);  
+function nextQuestion() {
+    newQuestion (randomQuestions[setQuestions]);
 
 }
 
+function newQuestion(question) {
+    questionsEl.innerText = question.question;
+    question.answers.forEach(answer => {
+        const button = document.createElement('button');
+        button.innerText = answer.text;
+        button.classList.add('btn')
+        
+        // if answer is correct 
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        
+    button.addEventListener("click", chooseAnswer)
+    answerButton.appendChild(button)
+    })
+}
+
+function resetAnswers() {
+    while (answerButton.firstChild) {
+        answerButton.removeChild (answerButton.firstChild);
+    }
+}
+
+function chooseAnswer(e) {
+
+}
+
+    
 // questions 
-var questions = [
-    new Question("Commonly used data types to NOT include:", ["Strings", "Booleans", "Alerts", "Numbers"],"Alerts"),
-    new Question("Commonly used data types to NOT include:", ["Strings", "Booleans", "Alerts", "Numbers"],"Alerts"),
-    new Question("Commonly used data types to NOT include:", ["Strings", "Booleans", "Alerts", "Numbers"],"Alerts"),
-    new Question("Commonly used data types to NOT include:", ["Strings", "Booleans", "Alerts", "Numbers"],"Alerts"),
-    new Question("Commonly used data types to NOT include:", ["Strings", "Booleans", "Alerts", "Numbers"],"Alerts"),
-    
-];
-
-
-// create new 
-var startQuiz = new startQuiz(questions);
-
-// run results 
-results();
-
-}
+const questions = [
+    {
+    question: "Commonly used data types to NOT include:",
+    answers: [
+        { text: "1. Strings", correct: false },
+        { text: "2. Booleans", correct: false },
+        { text: "3. Alerts", correct: true },
+        { text: "4. Numbers", correct: false }
+    ]
+}    
+]
