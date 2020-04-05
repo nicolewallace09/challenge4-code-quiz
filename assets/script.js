a56e782e5cf64ccedd86da2a7e44bf3513d443d7
nicolewallace09 <nicole.elisaw@gmail.com>
2020-04-05 15:04:18 -0700

    Fixed buttons + timer

--- assets/script.js
+++ assets/script.js
@@ -6,44 +6,57 @@ const answerButton = document.getElementById('choices')
 
 let randomQuestions, setQuestions
 
+var score = 0;
+
 startButton.addEventListener('click', startQuiz)
 
 // start quiz
+ var t = 75;
+ var counter = 0;
+ console.log(t);
+ t--;
+
 function startQuiz() {
     resetAnswers ();
     console.log("started")
-    //counterTime();
-    //timer = 60 ;
 
     startButton.classList.add('hide');
     startScreen.classList.add('hide');
     randomQuestions = questions.sort(() => Math.random() -.5)
     setQuestions = 0 
     questionsContainer.classList.remove('hide');
-    nextQuestion();
+    generateQuestion();
 }
 
-function nextQuestion() {
-    newQuestion (randomQuestions[setQuestions]);
+function generateQuestion() {
+    nextQuestion (randomQuestions[setQuestions]);
+    setQuestions++
 
 }
 
-function newQuestion(question) {
-    questionsEl.innerText = question.question;
-    question.answers.forEach(answer => {
-        const button = document.createElement('button');
+function nextQuestion(question) {
+        questionsEl.innerText = question.question;
+        question.answers.map((answer,index) => {
+        console.log(answer,index)
+        var button = document.createElement('button');
         button.innerText = answer.text;
         button.classList.add('btn')
-        
-        // if answer is correct 
-        if (answer.correct) {
-            button.dataset.correct = answer.correct
-        }
-        
-    button.addEventListener("click", chooseAnswer)
-    answerButton.appendChild(button)
-    })
+        //var button = document.querySelector(`.btn${index +1}`);
+        //button.innerHTML= answer.text;
+
+        //document.querySelector(".btn").addEventListener("click", chooseAnswer)
+        button.addEventListener("click", chooseAnswer(index))
+        answerButton.appendChild(button)
+
+    //return;
+    
+   })
 }
+
+function buttonOne () {
+    console.log(randomQuestions, "hello")
+}
+
 
 function resetAnswers() {
     while (answerButton.firstChild) {
@@ -51,8 +64,12 @@ function resetAnswers() {
     }
 }
 
-function chooseAnswer(e) {
-
+function chooseAnswer(index) {
+    console.log('choose answer', index)
+     // if answer is correct 
+     if (answerButton === true) {
+        console.log()
+     }
 }
 
     
@@ -66,5 +83,43 @@ const questions = [
         { text: "3. Alerts", correct: true },
         { text: "4. Numbers", correct: false }
     ]
-}    
+    },
+    {   
+    question: "The condition in an if / else statement is enclosed with____.",
+    answers: [
+        { text: "1. Quotes", correct: false },
+        { text: "2. Curly brackets", correct: true },
+        { text: "3. Parenthesis", correct: false },
+        { text: "4. Square brackets", correct: false }
+    ]
+    },   
+    {   
+    question: "Arrays in JavaScript can be used to store _____:",
+    answers: [
+        { text: "1. Numbers and strings", correct: false },
+        { text: "2. Other arrays", correct: false },
+        { text: "3. Booleans", correct: true },
+        { text: "4. All of the above", correct: false }
+    ]
+    },
+    {   
+    question: "String values must be enclosed within ______ when being assigned to variables.",
+    answers: [
+        { text: "1. Commas", correct: false },
+        { text: "2. Curly brackets", correct: false },
+        { text: "3. Quotes", correct: true },
+        { text: "4. Parenthesis", correct: false }
+    ]
+    },
+    {   
+    question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+    answers: [
+        { text: "1. JavaScript", correct: false },
+        { text: "2. Terminal/bash", correct: false },
+        { text: "3. For loop", correct: false },
+        { text: "4. Console.log", correct: true }
+    ]
+    }     
+    
 ]
+
